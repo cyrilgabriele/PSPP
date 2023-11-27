97874920e248f3bc1eef38e1be7f5ed5c140aad5
gabricyr <gabricyr@students.zhaw.ch>
2023-11-27 10:36:34 +0100

    finished PS13

--- 11_Praktikum_Python2/PS13.py
+++ 11_Praktikum_Python2/PS13.py
@@ -12,6 +12,12 @@ def assoc(_d, key, value):
     return d
 
 
+def call(fn, key):
+    def apply_fn(record):
+        return assoc(record, key, fn(record.get(key)))
+    return apply_fn
+
+
 def pipeline_each(data, fns):
     import functools
     return functools.reduce(lambda a, fn: list(map(fn, a)), fns, data)
@@ -38,6 +44,19 @@ if __name__ == '__main__':
              {'name': 'women', 'country': 'Germany', 'active': False},
              {'name': 'a silver mt. zion', 'country': 'Spain', 'active': True}]
 
+    print(pipeline_each(bands, [set_canada_as_country,
+                                strip_punctuation_from_name,
+                                capitalize_names]))
+
+    print(f"\nThis is bands, which should be unchanged because \
+    pipeline_each method acts on deep copies of bands due to functional approach: \n{bands}")
+
+    # Approach with call() method:
+    set_canada_as_country = call(lambda x: 'Canada', 'country')
+    strip_punctuation_from_name = call(lambda x: x.replace('.', ''), 'name')
+    capitalize_names = call(lambda x: x.title(), 'name')
+
+    print(f"\nThis is bands done with the call method approach:")
     print(pipeline_each(bands, [set_canada_as_country,
                                 strip_punctuation_from_name,
                                 capitalize_names]))
